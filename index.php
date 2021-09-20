3107dd73897686abebab88fb7ac224d2c9b90f8c
arradz <arradz@icloud.com>
2021-09-20 15:04:04 +0700

    menambhkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquam facere veritatis, eveniet incidunt nobis explicabo reprehenderit ipsum! Labore reprehenderit ad expedita distinctio ullam cumque officia illo ipsum qui modi?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
